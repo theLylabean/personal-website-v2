b150a22efc149d3c39006386889d591276599edb
Lyla Lynn <lyladlynn@gmail.com>
2025-02-19 21:59:29 -0600

    Finishing touches to javascrip, css, and html. Ready for submission.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 console.log('My cat Sunny was the best cat in the world! You can see him here: https://drive.google.com/drive/folders/1LAWXVgCwK_lHFiFfYHXw8YI_jEoyltly?usp=share_link');
 
-const form = document.querySelector('#form')
-const submitButton = document.querySelector('#submit')
+const form = document.querySelector('form')
+const submitButton = document.querySelector('submit')
 
 form.addEventListener('submit', (e) => {
   submitButton.disabled = true
--- success.js
+++ success.js
@@ -1,3 +1,3 @@
-setTimeout(() => {
-    window.location.href = window.location.origin
-  }, 5000)
+// setTimeout(() => {
+//    window.location.href = window.location.origin
+//  }, 5000)
